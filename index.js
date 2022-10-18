763d7de009293345015bfab47589a15d57d36f21
tamakiii <tamakiii@users.noreply.github.com>
2022-10-19 08:33:05 +0900

    init app

---
+++ index.js
@@ -0,0 +1,15 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+app.whenReady().then(() => {
+  const window = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+    }
+  })
+
+  window.loadFile('index.html')
+})
